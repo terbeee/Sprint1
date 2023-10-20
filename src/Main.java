124b7682c7bd3bf80dab3211a2b927d054a18825
terbeee <tobymccrae2@gmail.com>
2023-10-20 09:13:20 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Admin
+ * @date 20/10/2023
+ * @project Default (Template) Project
+ */// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+    }
+}
